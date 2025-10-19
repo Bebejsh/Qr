e9f36d54b7d2f9395f65bf2a909c6b50a240bd7a
Bebejsh <bmitri.culibaba@yandex.ru>
2025-10-20 03:12:36 +0700

    Add ucheb project

---
+++ qr1/qr1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace qr1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 94);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Засейвить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SeaGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(208, 410);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 94);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Вставить в ворд";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(28, 318);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Вставьте ссылку сюда";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.MouseEnter += new System.EventHandler(this.textBox1_MouseEnter);
+            this.textBox1.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(72, 344);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(202, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Генерировать";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(353, 506);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "QR ссылке";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ qr1/qr1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+using Application = Microsoft.Office.Interop.Word.Application;
+using System.Windows.Documents;
+using Section = Microsoft.Office.Interop.Word.Section;
+using Microsoft.Azure.Amqp.Framing;
+
+namespace qr1
+{
+    public partial class Form1 : Form
+    {
+        private bool isTextBoxEmpty = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.Filter = "Images|*.png;*.bmp;*.jpg";
+                saveDialog.Title = "Save an Image";
+
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(saveDialog.FileName);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var wordApplication = new Application();
+            var wordDocument = wordApplication.Documents.Add();
+            var sections = wordDocument.Sections;
+            // Получение пути к изображению
+            var imagePath = pictureBox1.ImageLocation;
+
+            // Вставка изображения в документ Word
+            var shape = wordDocument.Shapes.AddPicture(imagePath, false, true);
+
+
+            foreach (Section section in sections)
+            {
+                // Получение коллекции всех нижних колонтитулов раздела
+                var footers = section.Footers;
+
+                // Установка свойств нижнего колонтитула каждой страницы
+                foreach (Footer footer in footers)
+                {
+                    // Определение диапазона для нижнего колонтитула текущей страницы
+                    var range = footer.Range;
+
+                    // Размещение изображения внизу колонтитула текущей страницы
+                    var picture = range.InlineShapes.AddPicture(imagePath);
+                    picture.Height = 100; // Задайте нужную высоту изображения
+                    picture.Width = 100; // Задайте нужную ширину изображения
+                }
+            }
+
+            wordDocument.SaveAs("C:\\Users\\kab511students\\Desktop\\dvij — копия\\1233.docx" );
+
+            // Закрытие документа и выход из приложения Word
+            wordDocument.Close();
+            wordApplication.Quit();
+
+
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            if (isTextBoxEmpty)
+            {
+                textBox1.Text = "";
+                isTextBoxEmpty = false;
+            }
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            if (isTextBoxEmpty)
+            {
+                textBox1.Text = "Напиши сюда";
+                isTextBoxEmpty = true;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator Qg = new QRCodeGenerator();
+            var MyData = Qg.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.L);
+            var data = new QRCode(MyData);
+            pictureBox1.Image = data.GetGraphic(50);
+        }
+    }
+}
